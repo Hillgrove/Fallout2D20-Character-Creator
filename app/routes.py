36dfa7874f073782094b3f765f9d4c4851cf3604
Jesper Højlund <github@hillgrove.dk>
2024-07-10 22:35:10 +0200

    Added dashboard

--- app/routes.py
+++ app/routes.py
@@ -2,19 +2,22 @@
 from flask import render_template, url_for, flash, redirect
 from app import app, db
 from app.forms import RegistrationForm, LoginForm
-from app.models import User
+from app.models import User, Character
 from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return render_template("home.html")
+@app.route("/index")
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for("dashboard"))
+    return render_template("index.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for("homer"))
+        return redirect(url_for("index"))
     form = RegistrationForm()
     if form.validate_on_submit():
         # Hash the password using werkzeug
@@ -26,22 +29,31 @@ def register():
         return redirect(url_for("login"))
     return render_template("register.html", title="Register", form=form)
 
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for("home"))
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         # Verify the password using werkzeug
         if user and check_password_hash(user.password_hash, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for("home"))
+            return redirect(url_for("index"))
         else:
             flash("Login unsuccessful. Please check username and password", "danger")
     return render_template("login.html", title="Login", form=form)
 
+
 @app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for("home"))
+    return redirect(url_for("index"))
+
+
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    characters = Character.query.filter_by(user_id=current_user.id).all()
+    return render_template("dashboard.html", characters=characters)
